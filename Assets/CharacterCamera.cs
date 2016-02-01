794ca817627bd1442137c18007505c00d7202ecf
William <wap2895@rit.edu>
2016-02-01 15:16:09 -0500

    The first commit

---
+++ Assets/CharacterCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCamera : MonoBehaviour {
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = player.GetComponent<Transform> ().position;
+	}
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = this.transform.position;
+		position.z += .05f;
+		position.x += Input.GetAxis ("Horizontal") / 20;
+		this.transform.position = position;
+		//Debug.Log(this.transform.rotation);
+		this.transform.rotation = Quaternion.identity;
+		this.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+	}
+}
