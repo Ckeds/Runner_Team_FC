360f447b96d68823bce5da107e1544f74e8675be
WILLIAM POWELL (RIT Student) <wap2895@ad.rit.edu>
2016-02-10 14:58:28 -0500

    Added files to begin Procedural Map Generation

---
+++ Assets/MapManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void Setup (List<Obstacle> obs, ResourceManager r, bool d)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
