70f5f771c3fa0ec4f94ace9f2b02b7c1c8c2184b
DANNY LEE (RIT Student) <dxl1720@ad.rit.edu>
2016-02-24 16:44:57 -0500

    Added Fire

---
+++ Assets/Fire.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color color = GetComponent<MeshRenderer>().material.color;
+		color.r = 255.0f;
+		color.b = 0.0f;
+		color.g = 0.0f;
+		color.a = 0.5f;
+		GetComponent<MeshRenderer>().material.color = color;
+
+		float x = this.transform.localScale.x + 0.05f;
+		float z = this.transform.localScale.z + 0.05f;
+
+		this.transform.localScale = new Vector3(x,4,z);
+	}
+}
